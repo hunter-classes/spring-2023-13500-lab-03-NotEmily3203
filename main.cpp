0466a97055b6ce7f9948cf5ecf46c57854eb937c
NotEmily3203 <linemily62@gmail.com>
2023-02-16 11:41:14 -0500

    added function testing

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 // add your code as appropriate 
 #include <iostream>
-
+#include <fstream>
+#include <cstdlib>
+#include <climits>
+#include "reservoir.h"
+#include "reverseorder.h"
 int main()
 {
-  
-
-  
+  std::cout << "East basin storage on 05/20/2018: " << get_east_storage("05/20/2018") << " billion gallons" << std::endl;
+  std::cout << "East basin storage on 01/01/2018: " << get_east_storage("01/01/2018") << " billion gallons" << std::endl;
+  std::cout << "East basin storage on 02/07/2018: " << get_east_storage("02/07/2018") << " billion gallons" << std::endl;
+  std::cout << "minimum storage in East basin: " << get_min_east() << " billion gallons" << std::endl;
+  std::cout << "maximum storage in East basin: " << get_max_east() << " billion gallons" << std::endl;
+  std::cout << "09/13/2018:" << compare_basins("09/13/2018");
+  std::cout << "02/09/2018:" << compare_basins("02/09/2018");
+  reverse_order("05/29/2018", "06/02/2018");
+  reverse_order("01/01/2018", "01/10/2018");
   return 0;
 }
